7f6d5a7823e836e0b4b14cecbdfc5eed042b966b
Oluwayemisi Akinfesola <oluwayemisiakinfesola.bincom@gmail.com>
2023-09-01 20:03:02 +0100

    Cleaned Dataset, created dashboard.py

---
+++ clean_dataset.py
@@ -0,0 +1,7 @@
+import pygwalker as pyg
+import pandas as pd
+import streamlit.components.v1 as components
+import streamlit as st
+import numpy as np
+
+df = pd.read_excel("MK_estate_combined3_24_Aug_2023.xlsx")
